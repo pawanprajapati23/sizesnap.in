a61f758d48592d58e88a7ba27dd48ddc6bc97686
Pawan Prajapati <pavnkumarprajapati2000@gmail.com>
2026-07-11 22:37:41 +0530

    Implement Banner320x50 component with ad scripts
    Add ad banner script for 320x50 banner component.

--- components/Banner320x50.tsx
+++ components/Banner320x50.tsx
@@ -1,1 +1,26 @@
+"use client";
 
+import Script from "next/script";
+
+export default function Banner320x50() {
+  return (
+    <>
+      <Script id="adsterra-banner-config" strategy="afterInteractive">
+        {`
+          window.atOptions = {
+            key: 'f509bd7d24a58ce7a176067713ca61df',
+            format: 'iframe',
+            height: 50,
+            width: 320,
+            params: {}
+          };
+        `}
+      </Script>
+
+      <Script
+        src="https://www.highperformanceformat.com/f509bd7d24a58ce7a176067713ca61df/invoke.js"
+        strategy="afterInteractive"
+      />
+    </>
+  );
+}

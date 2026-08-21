b91521a23925042daeb6b2ddebeef10e2858731b
Pawan Prajapati <pawanprajapati23@users.noreply.github.com>
2026-08-22 01:45:21 +0530

    chore: remove temporary test files

--- script.js
+++
@@ -1,1 +0,0 @@
-// Dummy script just to use as a placeholder to structure thoughts if needed, or I can just edit the file directly using replace_file_content / write_to_file
--- test_oauth.js
+++
@@ -1,2 +0,0 @@
-// We cannot easily run this without valid oauth tokens in local env. 
-// But we can update the API route to fetch sites first!
